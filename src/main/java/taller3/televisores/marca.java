6293145547345cae8ef3c291df212c11026775d5
Vavasquezh <93843558+Vavasquezh@users.noreply.github.com>
2022-03-29 20:01:17 -0500

    Marca.java

--- src/main/java/taller3/televisores/marca.java
+++ src/main/java/taller3/televisores/marca.java
@@ -1,15 +1,17 @@
 package taller3.televisores;
+
 public class Marca {
 	private String nombre;
-	public Marca(String Nombre) {
-		this.nombre = Nombre;
+	public Marca(String nombre) {
+		this.nombre = nombre;
 	}
-	
-	public void setNombre(String Nombre) {
-		this.nombre = Nombre;
-	}
+
+
 	public String getNombre() {
-		return nombre;
+		return this.nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 }
 
